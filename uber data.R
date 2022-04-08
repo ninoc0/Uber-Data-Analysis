df1f5c7028e1c2c68ffd628dcf51a21c4976002d
Nico O'Neill <90474645+ninoc0@users.noreply.github.com>
2022-04-08 00:42:25 -0400

    Update uber data.R

--- uber data.R
+++ uber data.R
@@ -1,4 +1,4 @@
-install.packages("ggthemes")
+install.packages('ggthemes', dependencies = TRUE)
 install.packages("DT")
 install.packages("tinyverse")
 
@@ -12,14 +12,13 @@ library(readxl) #reads the excel files
 library(dplyr) #used in data manipulation
 
 library(tidyr) #cleans code
-library(tinyverse)
 library(DT) #uses the Java program datatables
 library(scales) #creates scales for mapped data
 
-colors = c("#d285e6","#85b9e6","#e6d685","#e68885","#8b85e6","#e685e4","#c7f2c2","#f2c2c9"")
+colors = c("#d285e6","#85b9e6","#e6d685","#e68885","#8b85e6","#e685e4","#c7f2c2","#f2c2c9")
 #sets the colors used in the plots
 
-apr <- read.csv ("C:\\Users\\nicoo\\OneDrive\\Desktop\\uber-raw-data-apr14.csv")
+apr <- read.csv("C:\\Users\\nicoo\\OneDrive\\Desktop\\uber-raw-data-apr14.csv")
 may <- read.csv("C:\\Users\\nicoo\\OneDrive\\Desktop\\uber-raw-data-may14.csv")
 jun <- read.csv("C:\\Users\\nicoo\\OneDrive\\Desktop\\uber-raw-data-jun14.csv")
 jul <- read.csv("C:\\Users\\nicoo\\OneDrive\\Desktop\\uber-raw-data-jul14.csv")
@@ -30,11 +29,8 @@ sep <- read.csv("C:\\Users\\nicoo\\OneDrive\\Desktop\\uber-raw-data-sep14.csv")
 data <- rbind(apr,may,jun,jul,aug,sep)
 cat(dim(data))
 #combines the data into one set and checks the dimensions
-
-data$Date.Time <- as.POSIXct(data$Date.Time, format = "%m/%d/%Y %H:%M:%S")
-
+data$Date.Time <- as.POSIXct(data$Date.Time, format="%m/%d/%Y %H:%M:%S")
 data$Time <- format(as.POSIXct(data$Date.Time, format = "%m/%d/%Y %H:%M:%S"), format="%H:%M:%S")
-
 data$Date.Time <- ymd_hms(data$Date.Time)
 
 data$day <- factor(day(data$Date.Time))
@@ -42,10 +38,9 @@ data$month <- factor(month(data$Date.Time, label = TRUE))
 data$year <- factor(year(data$Date.Time))
 data$dayofweek <- factor(wday(data$Date.Time, label = TRUE))
 
-data$hour <- factor(hour(hms(data$Time)))
-data$minute <- factor(minute(hms(data$Time)))
-data$second <- factor(second(hms(data$TIme)))
-#format of the date.time column and creating factors of time
+data$second = factor(second(hms(data$Time)))
+data$minute = factor(minute(hms(data$Time)))
+data$hour = factor(hour(hms(data$Time)))
 
 head(data)
 #check the data is correct
@@ -62,5 +57,14 @@ geom_bar(stat="identity",
 ggtitle("Trips Every Hour", subtitle = "aggregated today") +
   theme(legend.position = "none",
         plot.title = element_text(hjust = 0.5),
-        plot.subtitle = element_test(hjust = 0.5)) +
+        plot.subtitle = element_text(hjust = 0.5)) +
 scale_y_continuous(labels=comma)
+
+month_hour_data <- data %>% group_by(month, hour) %>% dplyr::summarise(Total = n())
+
+ggplot(month_hour_data, aes(hour, Total, fill=month)) +
+  geom_bar(stat = "identity") +
+  ggtitle("Trips by Hour and Month") +
+  scale_y_continuous(labels = comma)
+
+
